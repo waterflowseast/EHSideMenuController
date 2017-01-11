1d79c5fb7e739d4efde319faf5e95bf4016a9a66
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-11 13:22:48 +0800

    pod install for Example

--- Example/Pods/Target Support Files/EHSideMenuController/EHSideMenuController-umbrella.h
+++ Example/Pods/Target Support Files/EHSideMenuController/EHSideMenuController-umbrella.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
+#import "EHSideMenuController.h"
+#import "UIViewController+EHSideMenuController.h"
 
 FOUNDATION_EXPORT double EHSideMenuControllerVersionNumber;
 FOUNDATION_EXPORT const unsigned char EHSideMenuControllerVersionString[];
--- Example/Pods/Target Support Files/Pods-EHSideMenuController_Example/Pods-EHSideMenuController_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-EHSideMenuController_Example/Pods-EHSideMenuController_Example-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/EHSideMenuController/EHSideMenuController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/EHSideMenuController/EHSideMenuController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
 fi
